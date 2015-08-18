2fa6198686133f0ce094b7773cdfe7fc82104aec
IPv6 <ipv6@Nikolays-iMac.local>
2015-08-18 17:13:36 +0300

    old method removed

--- kingpin/KPClusteringController.h
+++ kingpin/KPClusteringController.h
@@ -44,17 +44,6 @@
  */
 - (void)refresh:(BOOL)animated;
 
-/**
- *  Refreshes the map annotations. The force flag allows the user to force a refresh even though the viewport
- *  has not significantly moved or if the map is not displayed. For most cases, leave the force flag to NO,
- *  forcing a refresh can be CPU heavy.
- *
- *  @param animated whether the view refresh is animated or not
- *  @param force    YES if you want to force a refresh without checking for viewport change or if the map
- *                  is visible, else NO.
- */
--(void)refresh:(BOOL)animated force:(BOOL)force;
-
 @end
 
 
